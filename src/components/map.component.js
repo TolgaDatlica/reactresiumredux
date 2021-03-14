a6cb3edd2afb8a03543e4fe1ce228bf7cf766f24
TolgaDatlica <m.tolgadatlica@gmail.com>
2021-03-14 19:08:04 +0300

    Update map.component.js
    rotation added

--- src/components/map.component.js
+++ src/components/map.component.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import Airplanes from './airplanes.component'
 import { Cartesian3 } from "cesium";
 import {
-    Viewer, CameraFlyTo
+    Viewer, CameraFlyTo, Camera
 } from "resium";
 const pointPosition = Cartesian3.fromDegrees(-0.136439, 51.507359, 8000000)
 class Map extends Component {
     constructor(props) {
         super(props);
     }
-
+    cameraChanged = (e) => {
+        console.log(e)
+    }
     render() {
         return (
             <Viewer
@@ -19,6 +21,7 @@ class Map extends Component {
                 baseLayerPicker={false} >
                 <Airplanes></Airplanes>
                 <CameraFlyTo duration={3} destination={pointPosition} />
+                <Camera onChange = { this.cameraChanged}/>
             </Viewer>
         );
     }
